507c838660a68ffdb3e0f0152beb40dbb41617c6
YP2410 <yaron.porat@campus.technion.ac.il>
2021-12-28 14:39:15 +0200

    flask is integrated with postgresql

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:porat2410@localhost/Namba'
+
+db=SQLAlchemy(app)
+
+class Student(db.Model):
+    __tablename__='users'
+    username=db.Column(db.String(40),primary_key=True)
+
+    def __init__(self,username):
+        self.username=username
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    username= request.form['username']
+    student=Student(username)
+    db.session.add(student)
+    db.session.commit()
+
+    return render_template('success.html', data=username)
+
+
+
+if __name__ == '__main__':  #python interpreter assigns "__main__" to the file you run
+    app.run(debug=True)
