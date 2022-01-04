e0a3e2ac6a4ac4b970d09e091cd8e2e4265861c9
Daniel_Meents <7707daniel@gmail.com>
2022-01-04 22:00:04 +0200

    whoopsi daisy

---
+++ backend/app.py
@@ -0,0 +1,172 @@
+from flask import Flask, render_template, request, send_from_directory, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import ForeignKey
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+CORS(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:porat2410@localhost/Namba'
+
+db=SQLAlchemy(app)
+
+class Student(db.Model):
+    __tablename__='users'
+    user_ID=db.Column(db.String(40), primary_key=True)
+    name=db.Column(db.String(40))
+    def __init__(self,username, name):
+        self.user_ID=username
+        self.name = name
+
+class Poll_ID(db.Model):
+    __tablename__='poll_id'
+    poll_ID=db.Column(db.Integer,primary_key=True)
+
+    def __init__(self):
+        self.poll_ID = 0
+
+
+class Admins(db.Model):
+    __tablename__='admins'
+    Username=db.Column(db.String(40),primary_key=True)
+    Password=db.Column(db.String(40))
+    def __init__(self,username, password):
+        self.Username = username
+        self.Password = password
+
+
+class Polls(db.Model):
+    __tablename__='polls'
+    poll_ID=db.Column(db.String(40),primary_key=True)
+    question=db.Column(db.String(255))
+    answers = db.Column(db.ARRAY(db.String(100)))
+    answers_counter = db.Column(db.ARRAY(db.Integer))
+    closed = db.Column(db.BOOLEAN)
+    multiple_choice = db.Column(db.BOOLEAN)
+    quiz = db.Column(db.BOOLEAN)
+    correct_answers = db.Column(db.ARRAY(db.Integer))
+    solution=db.Column(db.String(255))
+
+    def __init__(self,poll_ID, question, answers, answers_counter, closed, multiple_choice,
+                 quiz, correct_answers, solution):
+        self.poll_ID = poll_ID
+        self.question = question
+        self.answers = answers
+        self.answers_counter = answers_counter
+        self.closed = closed
+        self.multiple_choice = multiple_choice
+        self.quiz = quiz
+        self.correct_answers = correct_answers
+        self.solution = solution
+
+class Polls_answers(db.Model):
+    __tablename__='polls_answers'
+    poll_ID=db.Column(db.String(40), ForeignKey(Polls.poll_ID) ,primary_key=True)
+    user_ID=db.Column(db.String(40), ForeignKey(Student.user_ID) ,primary_key=True)
+    answers = db.Column(db.ARRAY(db.String(100)))
+    is_correct = db.Column(db.BOOLEAN)
+
+    def __init__(self,poll_ID, user_ID, answers, is_correct):
+        self.poll_ID = poll_ID
+        self.user_ID = user_ID
+        self.answers = answers
+        self.is_correct = is_correct
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+    # return send_from_directory(app.static_folder, 'build/index.html')
+    # return 1234
+
+@app.route('/mes', methods=['GET'])
+def message():
+    print("got to flask")
+    # return render_template("index.html")
+    # return send_from_directory(app.static_folder, 'build/index.html')
+    return {'message': '1234'}
+
+@app.route('/init_pollID', methods=['POST'])
+def init_pollID():
+
+    try:
+        poll_id=Poll_ID()
+        db.session.add(poll_id)
+        db.session.commit()
+    except Exception as e:
+        db.session.remove()
+        raise e
+
+
+@app.route('/submit', methods=['POST'])
+def submit(id, name):
+
+    try:
+        student=Student(id, name)
+        db.session.add(student)
+        db.session.commit()
+    except Exception as e:
+        db.session.remove()
+        raise e
+
+@app.route('/delete', methods=['POST'])
+def delete(id):
+    try:
+        Student.query.filter_by(user_ID=id).delete()
+        db.session.commit()
+    except Exception as e:
+        db.session.remove()
+        raise e
+
+
+@app.route('/add_admin', methods=['POST'])
+def add_admin(username, password):
+
+    try:
+        admin = Admins(username, password)
+        db.session.add(admin)
+        db.session.commit()
+    except Exception as e:
+        db.session.remove()
+        raise e
+
+
+@app.route('/delete_admin', methods=['POST'])
+def delete_admin(username):
+    try:
+        Admins.query.filter_by(Username=username).delete()
+        db.session.commit()
+    except Exception as e:
+        db.session.remove()
+        raise e
+
+@app.route('/add_poll', methods=['GET', 'POST'])
+def add_poll(question, answers, answers_counter, closed, multiple_choice,
+             quiz, correct_answers, solution):
+    Result=db.session.query(Poll_ID).all()
+    poll_id = Result[0].poll_ID
+
+    try:
+        poll = Polls(poll_id, question, answers, answers_counter, closed, multiple_choice,
+                     quiz, correct_answers, solution)
+        db.session.add(poll)
+        db.session.commit()
+    except Exception as e:
+        db.session.remove()
+        raise e
+
+    Result[0].poll_ID += 1
+    db.session.commit()
+
+@app.route('/delete_poll', methods=['POST'])
+def delete_poll(poll_id):
+    try:
+        Polls.query.filter_by(poll_ID=poll_id).delete()
+        db.session.commit()
+    except Exception as e:
+        db.session.remove()
+        raise e
+
+
+
+if __name__ == '__main__':  #python interpreter assigns "__main__" to the file you run
+    app.run(debug=True)
---
+++ backend/telegram_bot.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python
+# pylint: disable=C0116,W0613
+# This program is dedicated to the public domain under the CC0 license.
+
+"""
+Simple Bot to reply to Telegram messages.
+First, a few handler functions are defined. Then, those functions are passed to
+the Dispatcher and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+Usage:
+Basic Echobot example, repeats messages.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
+
+import logging
+
+import psycopg2.errors
+import sqlalchemy.exc
+from telegram import Update, ForceReply
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+
+# Enable logging
+from app import submit, delete, add_poll, add_admin, delete_admin, delete_poll
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+
+
+# Define a few command handlers. These usually take the two arguments update and
+# context.
+
+def remove(update: Update, context: CallbackContext) -> None:
+    user = update.effective_user
+    '''    try:
+        delete(str(user.id))
+    except Exception as e:
+        print("exception is - " + str(e))
+        print(type(e))
+        update.message.reply_markdown_v2(
+            'Error occured\.\n'
+            # reply_markup=ForceReply(selective=True),
+        )
+        return'''
+    delete(str(user.id))
+    update.message.reply_markdown_v2(
+        'You are now removed\.\n'
+        # reply_markup=ForceReply(selective=True),
+    )
+
+
+def register(update: Update, context: CallbackContext) -> None:
+    user = update.effective_user
+    try:
+        submit(str(user.id), user.name)
+        #add_poll("Is Daniel Balool", ["yes", "no", "obviously"], [0,0,0], 0, 0, 0, [0], "yes" )
+        #delete_admin("yaron")
+        #delete_poll("1")
+    except sqlalchemy.exc.IntegrityError as e:
+        print("exception is - " + str(e))
+        print(type(e))
+        update.message.reply_markdown_v2(
+            'Error occured\.\n You are already registred to the system\.\n'
+            # reply_markup=ForceReply(selective=True),
+        )
+        return
+    except Exception as e:
+        print("exception is - " + str(e))
+        print(type(e))
+        update.message.reply_markdown_v2(
+            'Error occured\.\n'
+            # reply_markup=ForceReply(selective=True),
+        )
+        return
+    update.message.reply_markdown_v2(
+        'You are now registered\.\n'
+        # reply_markup=ForceReply(selective=True),
+    )
+
+
+def start(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /start is issued."""
+    user = update.effective_user
+    update.message.reply_markdown_v2(
+        fr'Hello, {user.mention_markdown_v2()}\!'
+        # reply_markup=ForceReply(selective=True),
+    )
+
+    update.message.reply_markdown_v2(
+        'Welcome to smart polling\.\n'
+        'Please choose one of the options:'
+        # reply_markup=ForceReply(selective=True),
+    )
+
+    update.message.reply_markdown_v2(
+        '/register \- Register to start answering polls via telegram in smart polling system\n'
+        '/remove \- To stop getting polls queries\n in smart polling system\n'
+        '/start \- Use start anytime to see this menu again'
+    )
+
+
+def help_command(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+
+
+def echo(update: Update, context: CallbackContext) -> None:
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+
+
+def main() -> None:
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    updater = Updater("5029169709:AAHvskSVaIUTmMDeJW-6XBoOzi-IC4naEjA")
+
+    # Get the dispatcher to register handlers
+    dispatcher = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("register", register))
+    dispatcher.add_handler(CommandHandler("remove", remove))
+    dispatcher.add_handler(CommandHandler("help", help_command))
+
+    # on non command i.e message - echo the message on Telegram
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
